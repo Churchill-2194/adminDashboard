4e575136efe0370faf927ec0b0472118b528fc65
Churchill-2194 <churkakk@gmail.com>
2022-09-28 15:42:46 -0700

    updating adminDashboard

---
+++ editStudents.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<?php
+   require_once('includes/links.php');
+?>
+<body>
+	
+
+	<?php
+      require_once('includes/header.php');
+      require_once('includes/sidebar.php');
+
+      //Submit user data to database
+      //1. db connection
+      require_once('dbconnection.php');
+      if(isset($_POST{'submitEnrollments'})){
+        //2. fetch form data
+        $name = $_POST['name'];
+        $regnumber = $_POST['reg_number'];
+        $phone = $_POST['phone'];
+        $email = $_POST['email'];
+        $course = $_POST['course'];
+
+        //3.SQL: Query to insert data to database
+        $queryData = mysqli_query($conn, "INSERT INTO enrollments(name,reg_number,phone,email,course) VALUES('$name','$regnumber','$phone','$email','$course') ");
+        //4. check if data inserted
+        if($queryData){
+            echo "Data submitted successfully";
+        }
+        else{
+            echo "Error";
+        }
+      }
+    ?>  
+	
+	<div class="main-content">
+		<div class="container-fluid">
+			<div class="row">
+				
+				<div class="col-lg-12">
+					<div class="card">
+                        <div class="card-header bg-dark text-white text-centre">
+						      <span>Edit Students</span>
+					    </div>
+                        <div class="card-body">
+                            <form action="editStudents.php" method="post">
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <label for="name">Name</label>
+                                            <input type="text" class="form-control" name="name" id="">
+                                        </div>
+                                    </div>
+
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <label for="reg_number">Reg-Number</label>
+                                            <input type="text" class="form-control" name="reg_number" id="">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <label for="phone">Phone</label>
+                                            <input type="tel" class="form-control" name="phone" id="">
+                                        </div>
+                                    </div>
+
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <label for="email">Email</label>
+                                            <input type="email" class="form-control" name="email" id="">
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            <label for="course">Course</label>
+                                            <select class="form-control" name="course" id="">
+                                                <option value="">--select a course--</option>
+                                                <option value="Web Design & Development">Web Design & Development</option>
+                                                <option value="Android Application Development">Android Application Development</option>
+                                                <option value="Data Analysis">Data Analysis</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-lg-3" >
+                                        <div class="form-group">
+                                            <button type="submit" name="submitEnrollments" class="btn btn-success">update</button>
+                                        </div>
+                                    </div>
+                            </form>  
+                        </div>
+                    </div>
+				</div>
+				
+				
+			</div>
+
+			
+		</div>
+
+	</div>
+	<!-- All our code. write here   -->
+
+	
+<script src="jquery.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+</body>
+</html>
--- students.php
+++ students.php
@@ -9,6 +9,11 @@
 	<?php
       require_once('includes/header.php');
       require_once('includes/sidebar.php');
+
+      //fetch all user records
+      //1. database connection
+      require_once('dbconnection.php');
+      $fetchEnrolledStudents = mysqli_query($conn,"SELECT * FROM enrollments");
     ?>  
 	
 	<div class="main-content">
@@ -38,24 +43,29 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <td>1</td>
-                                    <td>Churchill</td>
-                                    <td>IN22/20269/22</td>
-                                    <td>+254798592877</td>
-                                    <td>churkakk@gmail.com</td>
-                                    <td>Web Design & Development</td>
-                                    <td>6th May 2022</td>
-                                    <td>
-                                        <a href="" class="btn btn-primary btn-sm">
-                                            <i class="fa fa-edit"></i>
-                                        </a>
-                                        <a href="" class="btn btn-success btn-sm">
-                                            <i class="fa fa-eye"></i>
-                                        </a>
-                                        <a href="" class="btn btn-danger btn-sm">
-                                            <i class="fa fa-trash"></i>
-                                        </a>
-                                    </td>
+                                    <?php while($row = mysqli_fetch_array($fetchEnrolledStudents)) { ?>
+                                        <tr>
+                                              <td><?php echo $row['Id']?></td>
+                                              <td><?php echo $row['name']?></td>
+                                              <td><?php echo $row['reg_number']?></td>
+                                              <td><?php echo $row['phone']?></td>
+                                              <td><?php echo $row['email']?></td>
+                                              <td><?php echo $row['course']?></td>
+                                              <td><?php echo $row['created_at']?></td>
+                                              <td>
+                                                  <a href="" class="btn btn-primary btn-sm">
+                                                     <i class="fa fa-edit"></i>
+                                                  </a>
+                                                  <a href="" class="btn btn-success btn-sm">
+                                                      <i class="fa fa-eye"></i>
+                                                  </a>
+                                                  <a href="" class="btn btn-danger btn-sm">
+                                                     <i class="fa fa-trash"></i>
+                                                  </a>
+                                              </td>
+                                        </tr>
+
+                                     <?php } ?>   
                                 </tbody>
                             </table>
                         </div>
@@ -72,8 +82,8 @@
 	<!-- All our code. write here   -->
 
 	
-<script src="jquery.min.js"></script>
-<script src="bootstrap/js/bootstrap.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  <script src="jquery.min.js"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </body>
 </html>
